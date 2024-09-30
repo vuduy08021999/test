c806573417a65410daeb73eb55951cbf07d29e4e
vuduy08021999 <122140428+vuduy08021999@users.noreply.github.com>
2024-10-01 00:23:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,31 @@ const express = require('express');
 const si = require('systeminformation');
 const app = express();
 const port = 3000;
+let startTime;
+
+function formatRuntime(milliseconds) {
+  const days = Math.floor(milliseconds / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((milliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((milliseconds % (1000 * 60)) / 1000);
+  
+  return `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+}
+
+app.use((req, res, next) => {
+  if (!startTime) {
+    startTime = Date.now();
+  }
+  next();
+});
 
 app.get('/', (req, res) => {
   const clientIP = req.ip;
-  console.log(req.ip)
-  res.send(`Your IP address is: ${clientIP}, I wanna change this file`);
+  const runtime = Date.now() - startTime;
+  console.log(req.ip);
+  res.send(`Your IP address is: ${clientIP}, Runtime: ${formatRuntime(runtime)}`);
 });
+
 app.get('/vm-info', async (req, res) => {
   try {
     const cpuInfo = await si.cpu();
@@ -17,12 +36,14 @@ app.get('/vm-info', async (req, res) => {
       memory: memInfo,
     };
     
-    res.json(vmInfo);
+    const runtime = Date.now() - startTime;
+    res.json({ ...vmInfo, runtime: formatRuntime(runtime) });
   } catch (error) {
     console.error('Error retrieving VM information:', error);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
